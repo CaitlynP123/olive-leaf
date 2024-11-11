5ac6b7bd64dda9b39506bf63530e08322d0bf51e
CaitlynP123 <74350761+CaitlynP123@users.noreply.github.com>
2024-11-11 20:38:38 +0530

    Add files via upload

---
+++ js/admin-home.js
@@ -0,0 +1,42 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-analytics.js";
+import {
+  getDatabase,
+  ref,
+  set,
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC44lD3CIOOJkgnt_E_Hb_xmBpZe6T8HoE",
+  authDomain: "olive-leaf-ea098.firebaseapp.com",
+  projectId: "olive-leaf-ea098",
+  storageBucket: "olive-leaf-ea098.appspot.com",
+  messagingSenderId: "390739993530",
+  appId: "1:390739993530:web:dfcbbfdc6da24ade167383",
+  measurementId: "G-F3NNRRF6WZ",
+  databaseURL: "https://olive-leaf-ea098-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase(app);
+const auth = getAuth();
+
+function logout() {
+  signOut(auth)
+    .then(() => {
+      window.location.href = "index.html";
+      alert("Logged out successfully!");
+    })
+    .catch((error) => {
+      console.error("Error", error);
+    });
+}
+
+document.getElementById("logoutbtn").addEventListener("click", logout);
